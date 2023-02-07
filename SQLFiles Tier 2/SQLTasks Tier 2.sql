192594f586403de6c4fb57597222ed0aa709f80d
Syed Newaz <snewaz@casinocashtrac.com>
2023-02-06 18:10:40 -0600

    final commit

--- SQLFiles Tier 2/SQLTasks Tier 2.sql
+++ SQLFiles Tier 2/SQLTasks Tier 2.sql
@@ -35,8 +35,14 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
 
+SELECT name, membercost FROM `Facilities`
+WHERE  membercost > 0
 
 /* Q2: How many facilities do not charge a fee to members? */
+
+SELECT COUNT(membercost) AS COUNT_MEMBERS
+FROM Facilities
+WHERE  membercost = 0
 
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
@@ -44,9 +50,18 @@ where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
+SELECT facid, name, membercost, monthlymaintenance
+FROM Facilities
+HAVING membercost < .20 * monthlymaintenance
+
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
+
+
+SELECT *
+FROM Facilities
+WHERE facid IN (1,5)
 
 
 /* Q5: Produce a list of facilities, with each labelled as
@@ -54,15 +69,40 @@ Try writing the query without using the OR operator. */
 more than $100. Return the name and monthly maintenance of the facilities
 in question. */
 
+SELECT NAME, monthlymaintenance, CASE WHEN monthlymaintenance > 100 THEN 'expensive' 
+                                 ELSE 'cheap' END AS MonthlyLabel
+FROM Facilities
+
+
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
+
+
+SELECT firstname, surname
+from Members
+where joindate = (
+    select Max(joindate)
+    from Members
+    )
+
 
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
+
+SELECT sub.court, CONCAT( sub.firstname,  ' ', sub.surname ) AS name
+FROM (
+SELECT Facilities.name AS court, Members.firstname AS firstname, Members.surname AS surname
+FROM Bookings
+INNER JOIN Facilities ON Bookings.facid = Facilities.facid
+AND Facilities.name LIKE  'Tennis Court%'
+INNER JOIN Members ON Bookings.memid = Members.memid
+) sub
+GROUP BY sub.court, sub.firstname, sub.surname
+ORDER BY name
 
 
 /* Q8: Produce a list of bookings on the day of 2012-09-14 which
@@ -73,7 +113,37 @@ facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
 
 
+SELECT Facilities.name AS facility, CONCAT( Members.firstname,  ' ', Members.surname ) AS name, 
+CASE WHEN Bookings.memid =0
+THEN Facilities.guestcost * Bookings.slots
+ELSE Facilities.membercost * Bookings.slots
+END AS cost
+FROM Bookings
+INNER JOIN Facilities ON Bookings.facid = Facilities.facid
+AND Bookings.starttime LIKE  '2012-09-14%'
+AND (((Bookings.memid =0) AND (Facilities.guestcost * Bookings.slots >30))
+OR ((Bookings.memid !=0) AND (Facilities.membercost * Bookings.slots >30)))
+INNER JOIN Members ON Bookings.memid = Members.memid
+ORDER BY cost DESC
+
+
+
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
+
+SELECT * 
+FROM (
+SELECT Facilities.name AS facility, CONCAT( Members.firstname,  ' ', Members.surname ) AS name, 
+CASE WHEN Bookings.memid =0
+THEN Facilities.guestcost * Bookings.slots
+ELSE Facilities.membercost * Bookings.slots
+END AS cost
+FROM Bookings
+INNER JOIN Facilities ON Bookings.facid = Facilities.facid
+AND Bookings.starttime LIKE  '2012-09-14%'
+INNER JOIN Members ON Bookings.memid = Members.memid
+)sub
+WHERE sub.cost >30
+ORDER BY sub.cost DESC
 
 
 /* PART 2: SQLite
